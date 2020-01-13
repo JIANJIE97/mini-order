2cca1ce2116c063828cd62c8768612b82c34d517
JianJie <JianJieEmail@163.com>
2020-01-13 18:27:09 +0800

    配置http转为https

--- mini-order/miniorder-product/src/main/java/fun/jianjie/miniorder/MiniOrderProductApplication.java
+++ mini-order/miniorder-product/src/main/java/fun/jianjie/miniorder/MiniOrderProductApplication.java
@@ -1,14 +1,56 @@
 package fun.jianjie.miniorder;
 
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
 
 @SpringBootApplication
 @EnableTransactionManagement
-public class MiniOrderProductApplication {
+public class MiniOrderProductApplication{
     public static void main(String[] args) {
         SpringApplication.run(MiniOrderProductApplication.class,args);
     }
+
+    /**
+     * http重定向到https
+     * @return
+     */
+    @Bean
+    public ServletWebServerFactory servletContainer(){
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context){
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection securityCollection = new SecurityCollection();
+                securityCollection.addPattern("/*");
+                securityConstraint.addCollection(securityCollection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    private Connector httpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        //Connector监听的http的端口号
+        connector.setPort(8080);
+        connector.setSecure(false);
+        //监听到http的端口号后转向到的https的端口号
+        connector.setRedirectPort(7000);
+        return connector;
+    }
+
 }
--- mini-order/miniorder-product/src/main/java/fun/jianjie/miniorder/config/RestTemplateConfig.java
+++ mini-order/miniorder-product/src/main/java/fun/jianjie/miniorder/config/RestTemplateConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate(ClientHttpRequestFactory factory){
+
         return new RestTemplate(factory);
     }
 
